c67f9f11f747ca6f1492b07cc94219e96ae057b6
dianjuar <dianjuar@gmail.com>
2017-10-28 18:31:08 -0500

    Responsive in practice page

--- shortcodes/practice-subscribe.php
+++ shortcodes/practice-subscribe.php
@@ -10,14 +10,18 @@ function lco_render_subscribe_form () {
         action="https://learnc-online.us17.list-manage.com/subscribe/post?u=393885e0ffd63041600e8c862&amp;id=9bb7f7120f"
         _lpchecked="1">
         <div id="optinforms-form1" class="lco_practice_subscribe_container">
-            <!-- Email -->
-            <div class="mdl-textfield mdl-js-textfield">
-                <input class="mdl-textfield__input" name="EMAIL" type="email" id="mce-EMAIL">
-                <label class="mdl-textfield__label" for="mce-EMAIL"><?= __('Your Email', LCO_THEME) ?></label>
-            </div>
-            <!-- Subscribe button -->
-            <div id="optinforms-form1-button-container">
-                <input name="submit" id="optinforms-form1-button" value="SUBSCRIBE" type="submit" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
+            <div class="mdl-grid">
+                <div class="mdl-cell mdl-cell--8-col mdl-cell--8-col-tablet">
+                    <!-- Email -->
+                    <div class="mdl-textfield mdl-js-textfield">
+                        <input class="mdl-textfield__input" name="EMAIL" type="email" id="mce-EMAIL">
+                        <label class="mdl-textfield__label" for="mce-EMAIL"><?= __('Your Email', LCO_THEME) ?></label>
+                    </div>
+                </div>
+                <div class="mdl-cell mdl-cell--4-col mdl-cell--8-col-tablet">
+                    <!-- Subscribe button -->
+                    <input name="submit" id="optinforms-form1-button" value="SUBSCRIBE" type="submit" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
+                </div>
             </div>
         </div>
     </form>
--- shortcodes/render-practice-features.php
+++ shortcodes/render-practice-features.php
@@ -5,19 +5,19 @@ function lco_render_practice_features() {
 	?>
 
 	<div class="lco-practice-features-container mdl-grid">
-		<div class="mdl-cell mdl-cell--3-col">
+		<div class="mdl-cell mdl-cell--3-col mdl-cell--4-col-tablet mdl-cell--4-col-phone">
 			<img src="<?= get_stylesheet_directory_uri() . '/shortcodes/assets/img/no-install.svg' ?>" alt="<?=__('No Install', LCO_THEME) ?>">
 			<h3>No Install</h3>
 		</div>
-		<div class="mdl-cell mdl-cell--3-col">
+		<div class="mdl-cell mdl-cell--3-col mdl-cell--4-col-tablet mdl-cell--4-col-phone">
 			<img src="<?= get_stylesheet_directory_uri() . '/shortcodes/assets/img/practical.svg' ?>" alt="<?=__('Practical', LCO_THEME) ?>">
 			<h3>Practical</h3>
 		</div>
-		<div class="mdl-cell mdl-cell--3-col">
+		<div class="mdl-cell mdl-cell--3-col mdl-cell--4-col-tablet mdl-cell--4-col-phone">
 			<img src="<?= get_stylesheet_directory_uri() . '/shortcodes/assets/img/track-progress.svg' ?>" alt="<?=__('Track Progress', LCO_THEME) ?>">
 			<h3>Track Progress</h3>
 		</div>
-		<div class="mdl-cell mdl-cell--3-col">
+		<div class="mdl-cell mdl-cell--3-col mdl-cell--4-col-tablet mdl-cell--4-col-phone">
 			<img src="<?= get_stylesheet_directory_uri() . '/shortcodes/assets/img/simple.svg' ?>" alt="<?=__('Simple', LCO_THEME) ?>">
 			<h3>Simple</h3>
 		</div>
